e6c8adbe9c5da7281301ad8719c47839adb33231
Faiza Zardari <itsmenisa7@gmail.com>
2026-02-20 21:01:57 +0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student
+{
+    public static void main( String[] args){
+    String name="ali";
+    String id="ali03";
+    int marks=7;
+    System.out.println("student name:" + name);
+    System.out.println("id:" + id);
+    System.out.println("marks:" + marks);
+
+
+    
+
+}
+}
